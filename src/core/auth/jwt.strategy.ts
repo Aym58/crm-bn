02c770246eeeb116197e6f594b59ac2f72e9b973
Alex <aym58@ukr.net>
2022-12-28 22:02:41 +0200

    error .env corrected

--- src/core/auth/jwt.strategy.ts
+++ src/core/auth/jwt.strategy.ts
@@ -1,16 +1,21 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { config } from 'dotenv';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserDto } from '../user/dto/user.dto';
 import { AuthService } from './auth.service';
 import { JwtPayload } from './dto/jwt.dto';
+
+config();
+const configService = new ConfigService();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'SECRET_KEY',
+      secretOrKey: configService.get('SEKRET_KEY'),
     });
   }
 
