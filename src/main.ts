0144aece490229bb458accf2334570c3ad99e1e9
Alex <aym58@ukr.net>
2022-12-29 01:11:31 +0200

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
