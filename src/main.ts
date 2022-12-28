cbf0b5c7736fb763cfd8e6e672963021393ac296
Alex <aym58@ukr.net>
2022-12-29 01:33:03 +0200

    cors vercel added

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  });
 
   await app.listen(port);
 }
